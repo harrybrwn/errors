a1695af11c2553a27765a775a9cbb73b5388029e
Harrison Brown <harrybrown98@gmail.com>
2019-03-06 17:39:11 -0800

    'error handling for agile development', lol

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='errors',
+	version='0.1',
+	description='Error handling for agile development',
+	url='https://github.com/harrybrwn/errors',
+	author='Harrison Brown',
+	author_email='harrybrown98@gmail.com',
+	license='MIT',
+	packages=['errors'],
+)
